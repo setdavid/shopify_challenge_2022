814100ecc7e9d4f78df334f1f6fbb263bc722fc0
David Setiawan <setdavid1014@gmail.com>
2022-01-18 00:21:48 -0500

    added toggle navbar search

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-export const NAVBAR_HEIGHT = 60;
+export const NAVBAR_HEIGHT = 50;
 
 function Navbar() {
-    let navbarCSS = {
+    let [toggleSearch, setToggleSearch] = useState();
+
+    let navbarMainCSS = {
         height: `${NAVBAR_HEIGHT}px`
     }
 
+    let navbarSearchCSS = {
+        maxHeight: `${toggleSearch ? "100px" : "0px"}`
+    }
+
     return (
-        <div id="navbar" className="container-fluid" style={navbarCSS}>
-            <div className="row full-height">
+        <div id="navbar" className="container-fluid">
+            <div id="navbar-main" className="row" style={navbarMainCSS}>
                 <div id="logo" className="col-6">
                     <h3>
                         Spacestagram
                     </h3>
                 </div>
                 <div id="search-toggle" className="col-6">
-                    symbol
+                    <FontAwesomeIcon icon={faSearch} className="clickable" onClick={() => setToggleSearch(!toggleSearch)} />
                 </div>
+            </div>
+            <div id="navbar-search" className="row" style={navbarSearchCSS}>
+                {toggleSearch ? <div className="col-12">
+                    hello
+                </div> : ""}
             </div>
         </div>
     );
