e6511eeb1fd6a62bb544685114b0bd1e74d37885
David Setiawan <setdavid1014@gmail.com>
2022-01-18 03:31:58 -0500

    implemented working navbar search

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { apodRange } from "../js/utils";
 
 export const NAVBAR_HEIGHT = 60;
 
 function Navbar() {
     let [toggleSearch, setToggleSearch] = useState();
+    let [startDate, setStartDate] = useState("");
+    let [endDate, setEndDate] = useState("");
 
     let navbarMainCSS = {
         height: `${NAVBAR_HEIGHT}px`
     }
 
     let navbarSearchCSS = {
-        maxHeight: `${toggleSearch ? "100px" : "0px"}`
+        maxHeight: `${toggleSearch ? "100%" : "0px"}`
+    }
+
+    let handleSubmit = e => {
+        e.preventDefault();
+        apodRange(true, startDate, endDate);
+        setToggleSearch(false);
+        setStartDate("");
+        setEndDate("");
     }
 
     return (
@@ -29,7 +40,30 @@ function Navbar() {
             </div>
             <div id="navbar-search" className="row" style={navbarSearchCSS}>
                 {toggleSearch ? <div className="col-12">
-                    hello
+                    <div style={{ marginBottom: "20px" }}>
+                        Retrieve APODs from a date range:
+                    </div>
+                    <form style={{ height: "100%", width: "100%" }} onSubmit={handleSubmit}>
+                        <div className="row">
+                            <div className="col-12 col-lg-4">
+                                <label>Start Date</label>
+                                <input
+                                    type="date"
+                                    name="start_date"
+                                    onChange={e => { setStartDate(e.target.value) }} />
+                            </div>
+                            <div className="col-12 col-lg-4">
+                                <label>End Date</label>
+                                <input
+                                    type="date"
+                                    name="end_date"
+                                    onChange={e => { setEndDate(e.target.value) }} />
+                            </div>
+                            <div className="col-12 col-lg-4">
+                                <button>Enter</button>
+                            </div>
+                        </div>
+                    </form>
                 </div> : ""}
             </div>
         </div>
